8ab652df434e2d8d440c64a2b640e9c83a8df422
hollyhbrady <hollyhbrady@gmail.com>
2021-05-04 19:34:14 +0100

    Park Constructor

---
+++ models/park.js
@@ -0,0 +1,7 @@
+const Park = function (name, ticketPrice, dinoCollection){
+    this.name = name;
+    this.ticketPrice = ticketPrice;
+    this.dinoCollections = dinoCollections;
+};
+
+module.exports = Taxi;
